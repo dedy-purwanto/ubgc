90364b61f841820642e9d6933a2d56d871745045
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 12:04:54 +0800

    set update_profile to be login_required

--- ubgc/apps/profiles/urls.py
+++ ubgc/apps/profiles/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from .views import DetailView, UpdateView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='detail'),
-    url(r'^update/$', UpdateView.as_view(), name='update'),
+    url(r'^update/$', login_required(UpdateView.as_view()), name='update'),
 )
