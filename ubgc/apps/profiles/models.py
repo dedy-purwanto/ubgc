6bc20bdca31caebcfd2043b93c7d7d309285816e
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 10:54:04 +0800

    Added User to Profile and initial migration

--- ubgc/apps/profiles/models.py
+++ ubgc/apps/profiles/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from easy_thumbnails.fields import ThumbnailerImageField
 
 class Profile(models.Model):
 
+    user = models.ForeignKey(User)
     bio = models.TextField(blank=True, null=True)
     photo = ThumbnailerImageField(upload_to='institution/photo/%Y/%m/%d')
     date_added = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+
