437837ba9b1303d82f48217e1df514e1ed005eed
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 10:29:37 +0800

    Added Entry model

--- ubgc/apps/entries/models.py
+++ ubgc/apps/entries/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    embed_code = models.TextField()
+    tags = models.TextField()
