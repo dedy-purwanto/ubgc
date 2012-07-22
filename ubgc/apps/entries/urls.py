e0011c87dee52fb11e5e56e77a590fe196602e56
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 21:31:46 +0800

    Update class name in url as well

--- ubgc/apps/entries/urls.py
+++ ubgc/apps/entries/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import CreateView, UpdateView, DetailView, VoteCreateView
+from .views import EntryCreateView, EntryUpdateView, DetailView, VoteCreateView
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)/edit/$', login_required(UpdateView.as_view()), name='edit'),
+    url(r'^(?P<pk>\d+)/edit/$', login_required(EntryUpdateView.as_view()), name='edit'),
     url(r'^(?P<pk>\d+)/vote/$', login_required(VoteCreateView.as_view()), name='vote'),
     url(r'^(?P<pk>\d+)/play/(?P<slug>[-\w]+)/$', DetailView.as_view(), name='play'),
-    url(r'^new/$', login_required(CreateView.as_view()), name='new'),
+    url(r'^new/$', login_required(EntryCreateView.as_view()), name='new'),
 )
