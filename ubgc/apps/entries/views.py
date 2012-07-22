f4ecb2cd11481110134c34db90d10fc3960fd15a
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 13:57:37 +0800

    Add CreateView for Entry

--- ubgc/apps/entries/views.py
+++ ubgc/apps/entries/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+
+from .models import Entry
+from .forms import EntryForm
+
+class CreateView(CreateView):
+
+    model = Entry
+    form_class = EntryForm
+    template_name = 'entries/form.html'
+
+    def get_object(self, *args, **kwargs):
+        return self.request.user.profile
+
+    def get_success_url(self, *args, **kwargs):
+        messages.success(self.request, "Your entry has been submitted")
+        return reverse("profiles:detail", args=[self.request.user.pk])
