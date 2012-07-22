b723e8ccb0e378c2b5288dc8f336751f682047ee
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 11:35:50 +0800

    Get or create profile

--- ubgc/apps/profiles/management/commands/regenerate_profiles.py
+++ ubgc/apps/profiles/management/commands/regenerate_profiles.py
@@ -1,6 +1,16 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+from profiles.models import Profile
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        users = User.objects.all()
+        for user in users:
+            profile, created = Profile.objects.get_or_create(user=user)
+            if created:
+                self.stdout.write("Profile %s created" % user.username)
+            else:
+                self.stdout.write("Profile %s already exists" % user.username)
 
         self.stdout.write("Done")
