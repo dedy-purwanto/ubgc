a4f8a32eeb3f611b84e6e406a6c3d69b85acb8d5
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 21:19:04 +0800

    Registered VoteCreateView to urls

--- ubgc/apps/entries/urls.py
+++ ubgc/apps/entries/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import CreateView, UpdateView, DetailView
+from .views import CreateView, UpdateView, DetailView, VoteCreateView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/edit/$', login_required(UpdateView.as_view()), name='edit'),
+    url(r'^(?P<pk>\d+)/vote/$', login_required(VoteCreateView.as_view()), name='vote'),
     url(r'^(?P<pk>\d+)/play/(?P<slug>[-\w]+)/$', DetailView.as_view(), name='play'),
     url(r'^new/$', login_required(CreateView.as_view()), name='new'),
 )
