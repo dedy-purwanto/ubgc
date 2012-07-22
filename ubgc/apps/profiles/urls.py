1c432e84161bfb714cb0a3888ab0a86dbb63958f
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 21:40:44 +0800

    Register votes view to urls

--- ubgc/apps/profiles/urls.py
+++ ubgc/apps/profiles/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import DetailView, UpdateView,  SubmissionView
+from .views import DetailView, UpdateView,  SubmissionView, VoteView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='detail'),
     url(r'^update/$', login_required(UpdateView.as_view()), name='update'),
     url(r'^submissions/$', login_required(SubmissionView.as_view()), name='submissions'),
+    url(r'^votes/$', login_required(VoteView.as_view()), name='votes'),
 )
