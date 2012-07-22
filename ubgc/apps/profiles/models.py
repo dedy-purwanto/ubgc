80772e3685d4abbbece388d0dd7dcf2639205715
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 10:53:21 +0800

    Added Profile model

--- ubgc/apps/profiles/models.py
+++ ubgc/apps/profiles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from easy_thumbnails.fields import ThumbnailerImageField
 
-# Create your models here.
+class Profile(models.Model):
+
+    bio = models.TextField(blank=True, null=True)
+    photo = ThumbnailerImageField(upload_to='institution/photo/%Y/%m/%d')
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
