52f7a7fe3ca7100c9181e93a1b264f445e794c31
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 22:54:28 +0800

    Remove render_to_response as it's not needed

--- ubgc/apps/home/views.py
+++ ubgc/apps/home/views.py
@@ -4,10 +4,6 @@ class HomeView(TemplateView):
 
     template_name = "home/home.html"
 
-    def render_to_response(self, context, **response_kwargs):
-
-        return super(HomeView, self).render_to_response(context, **response_kwargs)
-
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
 
