7d18ad9b49341976d4838cc5d7c86fa21dbe1687
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 23:10:12 +0800

    Add top_entries and recent_entries to context

--- ubgc/apps/home/views.py
+++ ubgc/apps/home/views.py
@@ -1,10 +1,19 @@
 from django.views.generic import TemplateView
 
+from entries.models import Entry
+
 class HomeView(TemplateView):
 
     template_name = "home/home.html"
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
+
+        entries = Entry.objects.filter(disabled=False)
+        top_entries = entries.order_by('-num_votes')
+        recent_entries = entries.order_by('-date_modified')
+
+        context['top_entries'] = top_entries
+        context['recent_entries'] = recent_entries
 
         return context
