684590ba1f6fa5d829afcc96cddc1ef031ae02c8
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 10:33:55 +0800

    Add default unicode handler

--- ubgc/apps/entries/models.py
+++ ubgc/apps/entries/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Entry(models.Model):
+
     title = models.CharField(max_length=255)
     description = models.TextField()
     embed_code = models.TextField()
@@ -9,7 +10,15 @@ class Entry(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
+    def __unicode__(self):
+        return self.title
+
+
 class Vote(models.Model):
+
     entry = models.ForeignKey(Entry)
     user = models.ForeignKey(User)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s - %s" % (self.entry.title, self.user)
