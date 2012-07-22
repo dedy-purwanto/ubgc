5fad506225aa2986d0bce92e79b06f6466352d0c
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 15:16:04 +0800

    Add url for editing entry

--- ubgc/apps/entries/urls.py
+++ ubgc/apps/entries/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import CreateView
+from .views import CreateView, UpdateView
 
 urlpatterns = patterns('',
+    url(r'^(?P<pk>\d+)/edit/$', login_required(UpdateView.as_view()), name='edit'),
     url(r'^new/$', login_required(CreateView.as_view()), name='new'),
 )
