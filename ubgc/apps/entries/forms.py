f748017d837d0627c845f31e5a0b25c470b3810b
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 13:55:05 +0800

    Add EntryForm's ModelForm

--- ubgc/apps/entries/forms.py
+++ ubgc/apps/entries/forms.py
@@ -1,2 +1,14 @@
 from django import forms
 
+from .models import Entry
+
+class EntryForm(forms.ModelForm):
+
+
+    class Meta:
+        model = Entry
+        exclude = (
+                'user',
+                'date_added',
+                'date_modified',
+        )
