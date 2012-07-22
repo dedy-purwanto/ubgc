254d94cd95cefaf6ba3ae90f386efddb8e690522
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 09:58:25 +0800

    Render basic template

--- ubgc/apps/home/views.py
+++ ubgc/apps/home/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+
+    template_name = "home/home.html"
+
+    def render_to_response(self, context, **response_kwargs):
+
+        return super(HomeView, self).render_to_response(context, **response_kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        return context
