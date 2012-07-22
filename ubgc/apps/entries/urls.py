8341b65f60eaf4ebc37c52b97e6db43d80c9811d
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 16:43:42 +0800

    Register DetailView for playing

--- ubgc/apps/entries/urls.py
+++ ubgc/apps/entries/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import CreateView, UpdateView
+from .views import CreateView, UpdateView, DetailView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/edit/$', login_required(UpdateView.as_view()), name='edit'),
+    url(r'^(?P<pk>\d+)/play/(?P<slug>[-\w]+)/$', login_required(DetailView.as_view()), name='play'),
     url(r'^new/$', login_required(CreateView.as_view()), name='new'),
 )
