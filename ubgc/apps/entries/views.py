4d330bf11eb6516de7bdabcc337ad50e99339d0c
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 14:30:49 +0800

    Fix form_valid redirection

--- ubgc/apps/entries/views.py
+++ ubgc/apps/entries/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 from .models import Entry
 from .forms import EntryForm
@@ -11,8 +12,9 @@ class CreateView(CreateView):
     form_class = EntryForm
     template_name = 'entries/form.html'
 
-    def get_object(self, *args, **kwargs):
-        return self.request.user.profile
+    def form_valid(self, form):
+        self.object = form.save(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self, *args, **kwargs):
         messages.success(self.request, "Your entry has been submitted")
