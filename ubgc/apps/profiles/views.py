271256c06858e96f5a75f98bec609fcbde4031c0
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 11:19:55 +0800

    Add DetailView for Profile

--- ubgc/apps/profiles/views.py
+++ ubgc/apps/profiles/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.views.generic import DetailView
+
+from .models import Profile
+
+class DetailView(DetailView):
+
+    context_object_name = 'profile'
+    model = Profile
+    template_name = 'profiles/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        profile = context['profile']
+        return context
