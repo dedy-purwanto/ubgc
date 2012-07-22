49ab8a16b51a5237eebd102f0eefd4d6765f448d
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 11:11:37 +0800

    Add username and name linked to User model

--- ubgc/apps/profiles/forms.py
+++ ubgc/apps/profiles/forms.py
@@ -3,6 +3,9 @@ from .models import Profile
 
 class ProfileForm(forms.ModelForm):
 
+    username = forms.CharField(max_length=30)
+    name = forms.CharField(max_length=255)
+
     class Meta:
         model = Profile
     
