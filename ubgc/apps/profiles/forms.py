c35f30ae0df66fcd02058b4d8a3f9c074ddca656
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 11:14:16 +0800

    Add email field, pre fill username and name with username and first_name

--- ubgc/apps/profiles/forms.py
+++ ubgc/apps/profiles/forms.py
@@ -5,6 +5,18 @@ class ProfileForm(forms.ModelForm):
 
     username = forms.CharField(max_length=30)
     name = forms.CharField(max_length=255)
+    email_address = forms.EmailField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+        try:
+            if self.instance.pk:
+                self.fields['username'].initial = self.instance.user.username
+                self.fields['name'].initial = self.instance.user.first_name
+        
+        except Profile.DoesNotExist:
+            pass
 
     class Meta:
         model = Profile
