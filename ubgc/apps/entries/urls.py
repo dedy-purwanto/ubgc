818f7a1ede88a39697afbd59f93515f2f7b2aa14
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 22:18:22 +0800

    Register EntryDeleteView to urls

--- ubgc/apps/entries/urls.py
+++ ubgc/apps/entries/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
 from .views import EntryCreateView, EntryUpdateView, DetailView, \
-    VoteCreateView, VoteListView, VoteDeleteView
+    VoteCreateView, VoteListView, VoteDeleteView, EntryDeleteView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/edit/$', login_required(EntryUpdateView.as_view()), name='edit'),
     url(r'^(?P<pk>\d+)/vote/$', login_required(VoteCreateView.as_view()), name='vote'),
+    url(r'^(?P<pk>\d+)/delete/$', login_required(EntryDeleteView.as_view()), name='delete'),
     url(r'^(?P<pk>\d+)/play/(?P<slug>[-\w]+)/$', DetailView.as_view(), name='play'),
     url(r'^new/$', login_required(EntryCreateView.as_view()), name='new'),
     url(r'^votes/$', login_required(VoteListView.as_view()), name='votes'),
