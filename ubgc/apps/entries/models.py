ac881a800326942993d4577caccc250e5a910b33
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 10:29:59 +0800

    Fix typo

--- ubgc/apps/entries/models.py
+++ ubgc/apps/entries/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Entry(models.model):
+class Entry(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     embed_code = models.TextField()
