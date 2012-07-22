e47b027069e6cb0283cb273d9164ae6a4a72f760
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 11:52:15 +0800

    register update profile url

--- ubgc/apps/profiles/urls.py
+++ ubgc/apps/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
-from .views import DetailView
+from .views import DetailView, UpdateView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='detail'),
+    url(r'^update/$', UpdateView.as_view(), name='update'),
 )
