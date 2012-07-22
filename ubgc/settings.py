fdcaf9fa4e1e3fc9c9164778309b9a67a01c80bb
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 11:05:12 +0800

    Added default context processors

--- ubgc/settings.py
+++ ubgc/settings.py
@@ -59,6 +59,16 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'socialregistration.contrib.twitter.auth.TwitterAuth',
