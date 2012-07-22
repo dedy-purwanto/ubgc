9ee594a4878b775223b82794277998c0a24bd85b
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-22 21:57:09 +0800

    Add vote view to entry instead

--- ubgc/apps/entries/urls.py
+++ ubgc/apps/entries/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import EntryCreateView, EntryUpdateView, DetailView, VoteCreateView
+from .views import EntryCreateView, EntryUpdateView, DetailView, \
+    VoteCreateView, VoteListView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/edit/$', login_required(EntryUpdateView.as_view()), name='edit'),
     url(r'^(?P<pk>\d+)/vote/$', login_required(VoteCreateView.as_view()), name='vote'),
     url(r'^(?P<pk>\d+)/play/(?P<slug>[-\w]+)/$', DetailView.as_view(), name='play'),
     url(r'^new/$', login_required(EntryCreateView.as_view()), name='new'),
+    url(r'^votes/$', login_required(VoteListView.as_view()), name='votes'),
 )
