dc69e6389171c5d2e8fe00fdf2a75b1920d64781
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-25 15:04:48 +0800

    Add social_auth pipelines

--- ubgc/settings.py
+++ ubgc/settings.py
@@ -81,6 +81,16 @@ FILE_UPLOAD_HANDLERS = (
 
 ROOT_URLCONF = 'ubgc.urls'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_auth.backends.pipeline.social.social_auth_user',
+    'social_auth.backends.pipeline.associate.associate_by_email',
+    'social_auth.backends.pipeline.user.get_username',
+    'social_auth.backends.pipeline.user.create_user',
+    'social_auth.backends.pipeline.social.associate_user',
+    'social_auth.backends.pipeline.social.load_extra_data',
+    'social_auth.backends.pipeline.user.update_user_details',
+)
+
 TEMPLATE_DIRS = (
     PROJECT_PATH + '/templates',
 )
