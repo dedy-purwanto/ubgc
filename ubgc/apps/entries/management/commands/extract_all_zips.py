7db30cc18549fc4cf1770794a64cc700bdbd48ef
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-25 13:44:24 +0800

    Call extract_zip_file on all entries

--- ubgc/apps/entries/management/commands/extract_all_zips.py
+++ ubgc/apps/entries/management/commands/extract_all_zips.py
@@ -1,6 +1,14 @@
 from django.core.management.base import BaseCommand
 
+from entires.models import Entry
+
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        entries = Entry.objects.all()
+        
+        for entry in entries:
+            self.stdout.write("Extracting entry #%s..\n" % entry.pk)
+            entry.extract_zip_file()
+
         self.stdout.write("Done\n")
