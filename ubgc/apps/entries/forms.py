3ea4513bf3ad4ea32300f3927a93799a7da16c91
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-25 13:39:38 +0800

    Make sure the file is in zip format when uploaded

--- ubgc/apps/entries/forms.py
+++ ubgc/apps/entries/forms.py
@@ -1,3 +1,5 @@
+import magic
+
 from django import forms
 
 from .models import Entry, Screenshot, Vote
@@ -8,6 +10,17 @@ class EntryForm(forms.ModelForm):
         self.instance.user = user
         return super(EntryForm, self).save(*args, **kwargs)
 
+    def clean_zip_file(self, *args, **kwargs):
+        zip_file = self.cleaned_data['zip_file']
+
+        file_path = zip_file.temporary_file_path()
+        m = magic.Magic(mime=True)
+        mimetype = m.from_file(file_path)
+
+        if not mimetype == "application/zip":
+            raise forms.ValidationError("The file must be in zip format")
+
+        return zip_file
 
     class Meta:
 
