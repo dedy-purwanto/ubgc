70f88d47eeedcfcfa9bcb9ffbcd5d86f720d6ac6
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-25 15:17:41 +0800

    Registered core context processor

--- ubgc/settings.py
+++ ubgc/settings.py
@@ -67,6 +67,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
+    'cores.context_processors.common',
 )
 
 AUTHENTICATION_BACKENDS = (
