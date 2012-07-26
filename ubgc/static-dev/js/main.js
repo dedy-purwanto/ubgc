3ee8ecf5c1e8ddc0948f19c7990850f962804072
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-26 23:22:48 +0800

    Revert the height

--- ubgc/static-dev/js/main.js
+++ ubgc/static-dev/js/main.js
@@ -1,13 +1,9 @@
-first_time = true;
 function resizeFrame(container, url) {
     var transport = new easyXDM.Socket(/** The configuration */{
         remote: url,
         container: container,
         onMessage: function(message, origin){
-            if(first_time == true){
-                this.container.getElementsByTagName("iframe")[0].style.height = (message + 50) + "px";
-                first_time = false;
-            }
+            this.container.getElementsByTagName("iframe")[0].style.height = message + "px";
         }
     });
 }
